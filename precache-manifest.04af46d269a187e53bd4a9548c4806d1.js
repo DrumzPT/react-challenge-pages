87aeb2026dfe94bc875c2328605a410e0d5509b6
Goncalo Duarte <fc46362@alunos.fc.ul.pt>
2020-01-27 11:55:11 +0000

    Updates

---
+++ precache-manifest.04af46d269a187e53bd4a9548c4806d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "636a55d8d040f7b5722bb4bc617ecff3",
+    "url": "/react-challenge-pages/index.html"
+  },
+  {
+    "revision": "89c9cec7b3fea2a66471",
+    "url": "/react-challenge-pages/static/css/2.c5724e5e.chunk.css"
+  },
+  {
+    "revision": "0f076a013d7616ee1b12",
+    "url": "/react-challenge-pages/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "89c9cec7b3fea2a66471",
+    "url": "/react-challenge-pages/static/js/2.8ad60e90.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/react-challenge-pages/static/js/2.8ad60e90.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0f076a013d7616ee1b12",
+    "url": "/react-challenge-pages/static/js/main.6ee8202c.chunk.js"
+  },
+  {
+    "revision": "d56671ff65cfdb337cf6",
+    "url": "/react-challenge-pages/static/js/runtime-main.3973bb67.js"
+  }
+]);
